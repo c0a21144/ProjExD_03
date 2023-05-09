df08c26da4a696f11f3fc0e5dd2d1507535363d4
c0a21144 <c0a21144dd@edu.teu.ac.jp>
2023-05-09 18:21:08 +0900

    Issue対応

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -8,6 +8,7 @@ import pygame as pg
 WIDTH = 1600  # ゲームウィンドウの幅
 HEIGHT = 900  # ゲームウィンドウの高さ
 NUM_OF_BOMBS = 5  # 爆弾の数
+NUM_OF_EXPLOSION = 5
 
 
 def check_bound(area: pg.Rect, obj: pg.Rect) -> tuple[bool, bool]:
